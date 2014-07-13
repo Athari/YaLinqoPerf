8efe1dc29b851d827c3b5be47933ee3f3e7c6a47
Athari <athari.p@gmail.com>
2014-07-13 15:01:09 +0400

    Added benchmark grouping. Added dictionary tests.

--- benchmark.php
+++ benchmark.php
@@ -3,6 +3,7 @@
 require_once 'vendor/autoload.php';
 use YaLinqo\Enumerable as E;
 use Ginq\Ginq as G;
+use YaLinqo\Functions as F;
 
 function is_cli ()
 {
@@ -10,7 +11,7 @@ function is_cli ()
     return php_sapi_name() == 'cli' && isset($_SERVER['PROMPT']);
 }
 
-function benchmark ($count, $operation)
+function benchmark_operation ($count, $operation)
 {
     $time = microtime(true);
     $message = "Success";
@@ -30,28 +31,47 @@ function benchmark ($count, $operation)
     ];
 }
 
-function benchmark_linq ($name, $count, $opRaw, $opYaLinqo, $opGinq)
+function benchmark_array ($name, $count, $benches)
 {
-    $benches = [
-        ['name' => "Raw PHP", 'op' => $opRaw],
-        ['name' => "YaLinqo", 'op' => $opYaLinqo],
-        ['name' => "Ginq", 'op' => $opGinq],
-    ];
-    echo "$name: ";
+    $benches = E::from($benches)->select('[ "name" => $k, "op" => $v ]')->toArray();
+    echo "\n$name ";
     foreach ($benches as $k => $bench) {
-        $benches[$k] = array_merge($bench, benchmark($count, $bench['op']));
+        $benches[$k] = array_merge($bench, benchmark_operation($count, $bench['op']));
         echo ".";
     }
     if (is_cli())
         echo str_repeat(chr(8), 4) . "    ";
-    echo "\n";
+    echo "\n" . str_repeat("-", strlen($name)) . "\n";
     $min = E::from($benches)->where('$v["result"]')->min('$v["time"]');
     foreach ($benches as $bench)
-        echo "  " . str_pad($bench['name'], 10) .
+        echo "  " . str_pad($bench['name'], 35) .
             ($bench['result']
-                ? number_format($bench['time'], 6) . " sec   x" . number_format($bench['time'] / $min, 1)
-                : $bench['message'])
+                ? number_format($bench['time'], 6) . " sec   "
+                . "x" . number_format($bench['time'] / $min, 1) . " "
+                . ($bench['time'] != $min
+                    ? "(+" . number_format(($bench['time'] / $min - 1) * 100, 0) . "%)"
+                    : "(minimum)"
+                )
+                : "* " . $bench['message'])
             . "\n";
+}
+
+function benchmark_linq ($name, $count, $opRaw, $opYaLinqo, $opGinq)
+{
+    benchmark_array($name, $count, [
+        "Raw PHP" => $opRaw,
+        "YaLinqo" => $opYaLinqo,
+        "Ginq" => $opGinq,
+    ]);
+}
+
+function benchmark_linq_groups ($name, $count, $opsRaw, $opsYaLinqo, $opsGinq)
+{
+    benchmark_array($name, $count, E::from([
+        "Raw PHP" => $opsRaw,
+        "YaLinqo" => $opsYaLinqo,
+        "Ginq   " => $opsGinq,
+    ])->selectMany('$v', '$v', 'is_numeric($k2) ? $k1 : "$k1 [$k2]"')->toArray());
 }
 
 function calc_math ($i)
@@ -61,7 +81,7 @@ function calc_math ($i)
 
 function calc_array ($i)
 {
-    return ['a' => $i, 'b' => "$i", 'c' => $i + 100500, 'd' => array($i, $i, $i)];
+    return [ 'a' => $i, 'b' => "$i", 'c' => $i + 100500, 'd' => [ $i, $i, $i ] ];
 }
 
 function not_implemented ()
@@ -69,8 +89,8 @@ function not_implemented ()
     throw new Exception("Not implemented");
 }
 
-$ITER_MAX = 1000;
-$CALC_ARRAY = [];
+$ITER_MAX = 10000;
+$CALC_ARRAY = [ ];
 for ($i = 0; $i < $ITER_MAX; $i++)
     $CALC_ARRAY[] = calc_array($i);
 
@@ -124,7 +144,7 @@ benchmark_linq("Iterate over $ITER_MAX ints and calculate floats", 100,
 
 benchmark_linq("Generate array of $ITER_MAX sequental integers", 100,
     function () use ($ITER_MAX) {
-        $a = [];
+        $a = [ ];
         for ($i = 0; $i < $ITER_MAX; $i++)
             $a[] = $i;
     },
@@ -135,69 +155,144 @@ benchmark_linq("Generate array of $ITER_MAX sequental integers", 100,
         G::range(0, $ITER_MAX)->toArray();
     });
 
-benchmark_linq("Generate array of $ITER_MAX calculated floats - anonymous function", 100,
-    function () use ($ITER_MAX) {
-        $a = [];
-        for ($i = 0; $i < $ITER_MAX; $i++)
-            $a[] = calc_math($i);
-    },
-    function () use ($ITER_MAX) {
-        E::range(0, $ITER_MAX)->select(function ($i) { return calc_math($i); })->toArray();
-    },
-    function () use ($ITER_MAX) {
-        G::range(0, $ITER_MAX)->select(function ($i) { return calc_math($i); })->toArray();
-    });
+benchmark_linq_groups("Generate array of $ITER_MAX calculated floats", 100,
+    [
+        function () use ($ITER_MAX) {
+            $a = [ ];
+            for ($i = 0; $i < $ITER_MAX; $i++)
+                $a[] = calc_math($i);
+        },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                E::range(0, $ITER_MAX)->select(function ($i) { return calc_math($i); })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                E::range(0, $ITER_MAX)->select('calc_math($v)')->toArray();
+            },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                G::range(0, $ITER_MAX)->select(function ($i) { return calc_math($i); })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                not_implemented();
+            },
+    ]);
 
-benchmark_linq("Generate array of $ITER_MAX calculated floats - string lambda", 100,
-    function () use ($ITER_MAX) {
-        $a = [];
-        for ($i = 0; $i < $ITER_MAX; $i++)
-            $a[] = calc_math($i);
-    },
-    function () use ($ITER_MAX) {
-        E::range(0, $ITER_MAX)->select('calc_math($v)')->toArray();
-    },
-    function () use ($ITER_MAX) {
-        not_implemented();
-    });
+benchmark_linq_groups("Generate array of $ITER_MAX calculated arrays", 100,
+    [
+        function () use ($ITER_MAX) {
+            $a = [ ];
+            for ($i = 0; $i < $ITER_MAX; $i++)
+                $a[] = calc_array($i);
+        },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                E::range(0, $ITER_MAX)->select(function ($i) { return calc_array($i); })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                E::range(0, $ITER_MAX)->select('calc_array($v)')->toArray();
+            },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                G::range(0, $ITER_MAX)->select(function ($i) { return calc_array($i); })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                not_implemented();
+            },
+    ]);
 
-benchmark_linq("Generate array of $ITER_MAX calculated arrays - anonymous function", 100,
-    function () use ($ITER_MAX) {
-        $a = [];
-        for ($i = 0; $i < $ITER_MAX; $i++)
-            $a[] = calc_array($i);
-    },
-    function () use ($ITER_MAX) {
-        E::range(0, $ITER_MAX)->select(function ($i) { return calc_array($i); })->toArray();
-    },
-    function () use ($ITER_MAX) {
-        G::range(0, $ITER_MAX)->select(function ($i) { return calc_array($i); })->toArray();
-    });
+benchmark_linq_groups("Generate array of $ITER_MAX calculated values from array", 100,
+    [
+        function () use ($ITER_MAX, $CALC_ARRAY) {
+            $a = [ ];
+            foreach ($CALC_ARRAY as $v)
+                $a[] = $v['d'][0];
+        },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                E::from($CALC_ARRAY)->select(function ($v) { return $v["d"][0]; })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                E::from($CALC_ARRAY)->select('$v["d"][0]')->toArray();
+            },
+        "string property path" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                not_implemented();
+            },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                G::from($CALC_ARRAY)->select(function ($v) { return $v["d"][0]; })->toArray();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                not_implemented();
+            },
+        "string property path" =>
+            function () use ($ITER_MAX, $CALC_ARRAY) {
+                G::from($CALC_ARRAY)->select('[d][0]')->toArray();
+            },
+    ]);
 
-benchmark_linq("Generate array of $ITER_MAX calculated arrays - string lambda", 100,
-    function () use ($ITER_MAX) {
-        $a = [];
-        for ($i = 0; $i < $ITER_MAX; $i++)
-            $a[] = calc_array($i);
-    },
-    function () use ($ITER_MAX) {
-        E::range(0, $ITER_MAX)->select('calc_array($v)')->toArray();
-    },
-    function () use ($ITER_MAX) {
-        not_implemented();
-    });
+/////////////////
+// Dictionaries /
+/////////////////
 
-benchmark_linq("Generate array of $ITER_MAX calculated values from array - property path", 100,
-    function () use ($ITER_MAX, $CALC_ARRAY) {
-        $a = [];
-        foreach ($CALC_ARRAY as $v)
-            $a[] = $v['d'][0];
-    },
-    function () use ($ITER_MAX, $CALC_ARRAY) {
-        E::from($CALC_ARRAY)->select('$v["d"][0]')->toArray();
-    },
-    function () use ($ITER_MAX, $CALC_ARRAY) {
-        G::from($CALC_ARRAY)->select('[d][0]')->toArray();
-    });
+benchmark_linq_groups("Generate lookup of 100 floats, calculate sum", 100,
+    [
+        function () use ($ITER_MAX) {
+            $dic = [ ];
+            for ($i = 0; $i < $ITER_MAX; $i++)
+                $dic[(string)tan($i % 100)][] = $i % 2 ? sin($i) : cos($i);
+            $sum = 0;
+            foreach ($dic as $g)
+                foreach ($g as $v)
+                    $sum += $v;
+            return $sum;
+        },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                $dic = E::range(0, $ITER_MAX)->toLookup(
+                    function ($v) { return (string)tan($v % 100); },
+                    function ($v) { return $v % 2 ? sin($v) : cos($v); });
+                return E::from($dic)->selectMany(F::$value)->sum();
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                $dic = E::range(0, $ITER_MAX)->toLookup('(string)tan($v % 100)', '$v % 2 ? sin($v) : cos($v)');
+                return E::from($dic)->selectMany(F::$value)->sum();
+            },
+    ],
+    [
+        "anonymous function" =>
+            function () use ($ITER_MAX) {
+                $dic = G::range(0, $ITER_MAX)->toLookup(
+                    function ($v) { return (string)tan($v % 100); },
+                    function ($v) { return $v % 2 ? sin($v) : cos($v); });
+                return G::from($dic)->selectMany(F::$value, F::$key)->sum() - 0.5623790762907; // WTF! extra 11th element
+            },
+        "string lambda" =>
+            function () use ($ITER_MAX) {
+                not_implemented();
+            },
+    ]);
 
-echo "Done!";
+echo "\nDone!\n";
